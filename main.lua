3066bcdccf266658c144c6e86ff099318dfbd785
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-17 13:26:54 -0800

    create Rect

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ local length = 10
 local x = 100
 local y = 100
 
+
 local function createRect()
   rect = {}
   rect.width = 10
@@ -19,17 +20,26 @@ local function createRect()
   table.insert(rectangles, rect)
 end
 
+local function Rect()
+  return {
+    width = 10,
+    length = 10,
+    x = 100,
+    y = 100,
+  }
+end
+
 function love.load()
-  rectangles = {}
-  tick = require"tick"
+  -- rectangles = {}
+  -- tick = require"tick"
 
   -- card = love.graphics.newImage("creamboi.png")
 
-  createSquare = false
+  -- createSquare = false
 
-  tick.delay(function () createSquare = true end, 2)
+  -- tick.delay(function () createSquare = true end, 2)
 
-  createRect()
+  player1 = Rect()
 end
 
 function love.keypressed(key)
@@ -39,35 +49,38 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-  print("X= ", rect.x, "Y= ", rect.y)
+  print("X= ", player1.x, "Y= ", player1.y)
 
-  tick.update(dt)
+  -- tick.update(dt)
+
+  local player1 = Rect()
+
 
   if love.keyboard.isDown("d") then
-    rect.x = rect.x + speed * dt
+    player1.x = player1.x + speed * dt
   elseif love.keyboard.isDown("a") then
-    rect.x = rect.x - speed * dt
+    player1.x = player1.x - speed * dt
   elseif love.keyboard.isDown("s") then
-    rect.y = rect.y + speed * dt
+    player1.y = player1.y + speed * dt
   elseif love.keyboard.isDown("w") then
-    rect.y = rect.y - speed * dt
+    player1.y = player1.y - speed * dt
   end
 
-  if rect.x < 0 then
-    rect.x = 1
-  elseif rect.x > 790 then
-    rect.x = 789
-  elseif rect.y < 0 then
-    rect.y = 1
-  elseif rect.y > 590 then
-    rect.y = 589
+  if player1.x < 0 then
+    player1.x = 1
+  elseif player1.x > 790 then
+    player1.x = 789
+  elseif player1.y < 0 then
+    player1.y = 1
+  elseif player1.y > 590 then
+    player1.y = 589
   end
 end
 
 function love.draw()
-  love.graphics.rectangle('fill', rect.x, rect.y, rect.length, rect.width)
+  love.graphics.rectangle('fill', player1.x, player1.y, player1.length, player1.width)
   -- love.graphics.draw(card, 200, 200)
-  if rect.x > 100 then
+  if player1.x > 100 then
     love.graphics.print("hi!", 100, 100)
   end
 
